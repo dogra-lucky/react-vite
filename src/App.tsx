de09782f5df72b710ca61963b786aacf5011200b
HarmanKahlon7 <you@example.com>
2026-02-18 11:43:46 +0530

    use library for error boundaries

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,29 @@ import {Toaster} from "react-hot-toast";
 
 import "./App.css";
 import {AppRoutes} from "./app/routes";
-import {ErrorBoundary} from "./components/ErrorBoundary";
+// import {ErrorBoundary} from "./components/ErrorBoundary";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
     <>
-      <ErrorBoundary
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+    
+      {/* <ErrorBoundary
         fallback={
           <div>
-            <h1>Something went wrong</h1>
-            <button onClick={() => window.location.reload()}>Reload</button>
+          <h1>Something went wrong</h1>
+          <button onClick={() => window.location.reload()}>Reload</button>
           </div>
-        }
-        onError={(error, errorInfo) => {
-          console.error("ErrorBoundary caught:", error, errorInfo);
-        }}
-      >
+          }
+          onError={(error, errorInfo) => {
+            console.error("ErrorBoundary caught:", error, errorInfo);
+            }}
+            > */}
         <AppRoutes />
         <Toaster position="top-right" />
-      </ErrorBoundary>
+            </ErrorBoundary>
+      {/* </ErrorBoundary> */}
     </>
   );
 }
